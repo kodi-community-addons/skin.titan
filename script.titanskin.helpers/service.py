13f918d5b28a7522ad1c97353ccc406dc335cddb
Marcel van der Veldt <m.vanderveldt@outlook.com>
2014-11-02 16:06:20 +0100

    fixed script error for non mb users

--- script.titanskin.helpers/service.py
+++ script.titanskin.helpers/service.py
@@ -10,10 +10,11 @@ import random
 import time
 import os
 
-__settings__ = xbmcaddon.Addon(id='plugin.video.xbmb3c')
-__cwd__ = __settings__.getAddonInfo('path')
-BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources/lib' ) )
-sys.path.append(BASE_RESOURCE_PATH)
+if xbmc.getCondVisibility("System.HasAddon(plugin.video.xbmb3c)"):
+    __settings__ = xbmcaddon.Addon(id='plugin.video.xbmb3c')
+    __cwd__ = __settings__.getAddonInfo('path')
+    BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources/lib' ) )
+    sys.path.append(BASE_RESOURCE_PATH)
 
 class TitanThread ():
 
