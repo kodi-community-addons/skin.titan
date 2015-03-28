f7dd18e2a09842bc0f6b95258274c75901f2e233
Marcel van der Veldt <m.vanderveldt@outlook.com>
2015-03-28 20:23:51 +0100

    update texturebuilder with new path

--- build_textures/TexturePacker-Build.bat
+++ build_textures/TexturePacker-Build.bat
@@ -14,8 +14,8 @@ Echo exclude.txt>>exclude.txt
 ECHO ----------------------------------------
 ECHO Creating XBT File...
 
-START /B /WAIT TexturePacker -dupecheck -input ..\skin.titan\media\ -output ..\skin.titan\media\Textures.xbt
-START /B /WAIT TexturePacker -dupecheck -input ..\skin.titan\themes\classic\ -output ..\skin.titan\media\classic.xbt
+START /B /WAIT TexturePacker -dupecheck -input ..\skin.titan.helix\media\ -output ..\skin.titan.helix\media\Textures.xbt
+START /B /WAIT TexturePacker -dupecheck -input ..\skin.titan.helix\themes\classic\ -output ..\skin.titan.helix\media\classic.xbt
 
 copy /Y ..\skin.titan\media\Textures.xbt ..\skin.titan.helix\media\Textures.xbt
 copy /Y ..\skin.titan\media\classic.xbt ..\skin.titan.helix\media\classic.xbt
