a34ccd1497df2a2607adf8f5d096814edc6f3c60
Marcel van der Veldt <m.vanderveldt@outlook.com>
2015-08-13 22:58:41 +0200

    migrate titan skin helper to new script.skin.helper.service

--- script.titanskin.helpers/default.py
+++ script.titanskin.helpers/default.py
@@ -1,5 +1,4 @@
 import xbmcaddon
-import xbmcplugin
 import os
 
 __settings__ = xbmcaddon.Addon(id='script.titanskin.helpers')
@@ -37,91 +36,11 @@ except:
     pass  
     
 # select action
-if action == "SENDCLICK":
-    MainModule.sendClick(argument1)
-
-elif action =="ADDSHORTCUT":
+if action =="ADDSHORTCUT":
     MainModule.addShortcutWorkAround()
 
-elif action == "SHOWINFO":
-    MainModule.showInfoPanel()
-
-#setwidget is called from window other then home    
-elif action == "SETWIDGET":
-    if (xbmc.getCondVisibility("!Window.IsActive(home)")):
-        from HomeMonitor import HomeMonitor
-        HomeMonitor().setWidget(argument1)
 elif action == "DEFAULTSETTINGS":
     MainModule.defaultSettings()
-elif action == "MUSICSEARCH":
-    MainModule.musicSearch()
-elif action == "SETVIEW":
-    MainModule.setView()
-elif action == "SEARCHTRAILER":
-    MainModule.searchTrailer(argument1)
-elif action == "SETFORCEDVIEW":
-    MainModule.setForcedView(argument1)    
-elif action == "ENABLEVIEWS":
-    MainModule.enableViews()
-elif action == "VIDEOSEARCH":
-    from SearchDialog import SearchDialog
-    searchDialog = SearchDialog("script-titanskin_helpers-CustomSearch.xml", __cwd__, "default", "1080i")
-    searchDialog.doModal()
-    del searchDialog
-elif action == "COLORPICKER":
-    from ColorPicker import ColorPicker
-    colorPicker = ColorPicker("script-titanskin_helpers-ColorPicker.xml", __cwd__, "default", "1080i")
-    colorPicker.skinString = argument1
-    colorPicker.doModal()
-    del colorPicker
-elif action == "COLORTHEMES":
-    from ColorThemes import ColorThemes
-    colorThemes = ColorThemes("script-titanskin_helpers-ColorThemes.xml", __cwd__, "default", "1080i")
-    colorThemes.doModal()
-    del colorThemes
-elif action == "CREATECOLORTHEME":
-    import ColorThemes as colorThemes
-    colorThemes.createColorTheme()
-elif action == "RESTORECOLORTHEME":
-    import ColorThemes as colorThemes
-    colorThemes.restoreColorTheme()
-elif action == "COLORTHEMETEXTURE":    
-    MainModule.selectOverlayTexture()
-elif action == "BUSYTEXTURE":    
-    MainModule.selectBusyTexture()     
-elif action == "BACKUP":
-    import BackupRestore
-    BackupRestore.backup()
-elif action == "RESTORE":
-    import BackupRestore
-    BackupRestore.restore()
-elif action == "RESET":
-    import BackupRestore
-    BackupRestore.reset()
-elif action == "SETSKINVERSION":
-    import Utils as utils
-    utils.setSkinVersion()
-elif "NEXTEPISODES" in argument1:
-    MainModule.getNextEpisodes()
-elif "RECOMMENDEDMOVIES" in argument1:
-    MainModule.getRecommendedMovies()
-elif "RECOMMENDEDMEDIA" in argument1:
-    MainModule.getRecommendedMedia(False)
-elif "RECENTMEDIA" in argument1:
-    MainModule.getRecentMedia()
-elif "SIMILARMOVIES" in argument1:
-    MainModule.getSimilarMovies()
-elif "INPROGRESSMEDIA" in argument1:
-    MainModule.getRecommendedMedia(True)      
-elif "FAVOURITEMEDIA" in argument1:
-    MainModule.getFavouriteMedia() 
-elif argument1 == "?FAVOURITES":
-    MainModule.getFavourites()
-elif "?LAUNCHAPP" in argument1:
-    try:
-        app = argument1.split("&&&")[-1]
-        xbmc.executebuiltin(app)
-    except: pass
 
 
     
--- script.titanskin.helpers/service.py
+++ script.titanskin.helpers/service.py
@@ -9,45 +9,115 @@ import xbmcaddon
 __settings__ = xbmcaddon.Addon(id='script.titanskin.helpers')
 __cwd__ = __settings__.getAddonInfo('path')
 __addonversion__ = __settings__.getAddonInfo('version')
-BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) )
-sys.path.append(BASE_RESOURCE_PATH)
 
-from BackgroundsUpdater import BackgroundsUpdater
-from LibraryMonitor import LibraryMonitor
-from LibraryMonitor import Kodi_Monitor
-from HomeMonitor import HomeMonitor
 
 class Main:
     
+    win = None
+    
     def __init__(self):
         
-        win = xbmcgui.Window( 10000 )
-        win.setProperty("titanhelper.version",__addonversion__.replace(".",""))
-        skin = xbmc.getSkinDir()
-        skinLabel = xbmcaddon.Addon(id=skin).getAddonInfo('name')
-        skinVersion = xbmcaddon.Addon(id=skin).getAddonInfo('version')
-        win.setProperty("skinTitle",skinLabel + " - " + xbmc.getLocalizedString(19114) + ": " + skinVersion)
-        win.setProperty("skinVersion",xbmc.getLocalizedString(19114) + ": " + skinVersion)
-        
-        KodiMonitor = Kodi_Monitor()
-        homeMonitor = HomeMonitor()
-        backgroundsUpdater = BackgroundsUpdater()
-        libraryMonitor = LibraryMonitor()
-                   
-        #start the extra threads
-        homeMonitor.start()
-        backgroundsUpdater.start()
-        libraryMonitor.start()
-        
+        listItem = None
+        lastListItem = None
+        mainMenuContainer = "300"
+        self.win = xbmcgui.Window( 10000 )
+
         while not (KodiMonitor.abortRequested() or xbmc.abortRequested):
+                        
+            # monitor main menu when home is active
+            if (xbmc.getCondVisibility("Window.IsActive(home) + !Window.IsActive(fullscreenvideo)")):
+
+                #monitor widget window prop
+                if self.win.getProperty("ShowWidget") == "show" and not xbmc.getCondVisibility("Window.IsActive(selectdialog) | Window.IsActive(shutdownmenu) | Window.IsActive(contextmenu)"):
+                    self.showWidget()
+                
+                listItem = xbmc.getInfoLabel("Container(%s).ListItem.Label" %mainMenuContainer)
+                if ((listItem != lastListItem) and xbmc.getCondVisibility("!Window.IsActive(selectdialog) + !Window.IsActive(shutdownmenu) + !Window.IsActive(contextmenu)")):
+                    
+                    # update the widget content
+                    if (xbmc.getCondVisibility("!Skin.HasSetting(DisableAllWidgets) + !Skin.String(GadgetRows, 3)")):
+                        
+                        #spotlight widget
+                        if xbmc.getCondVisibility("Skin.String(GadgetRows, enhanced)"):
+                            self.setSpotlightWidget(mainMenuContainer)
+                        
+                        #normal widget
+                        self.setWidget(mainMenuContainer)
+
+                    lastListItem = listItem
+  
+
             xbmc.sleep(150)
+
+    def logMsg(self, msg, level = 1):
+        doDebugLog = False
+        if doDebugLog == True or level == 0:
+            xbmc.log("Titanskin DEBUG --> " + msg)
+    
+    def showWidget(self):
+        linkCount = 20
+        while linkCount != 0 and not xbmc.getCondVisibility("ControlGroup(77777).HasFocus"):
+            xbmc.executebuiltin('Control.SetFocus(77777,0)')
+            linkCount -= 1
+            xbmc.sleep(50)
+    
+    def setWidget(self, containerID):
+        self.win.clearProperty("activewidget")
+        self.win.clearProperty("customwidgetcontent")
+        skinStringContent = ""
+        customWidget = False
+        
+        # workaround for numeric labels (get translated by xbmc)
+        skinString = xbmc.getInfoLabel("Container(" + containerID + ").ListItem.Property(submenuVisibility)")
+        skinString = skinString.replace("num-","")
+        if xbmc.getCondVisibility("Skin.String(widget-" + skinString + ')'):
+            skinStringContent = xbmc.getInfoLabel("Skin.String(widget-" + skinString + ')')
+        
+        # normal method by getting the defaultID
+        if skinStringContent == "":
+            skinString = xbmc.getInfoLabel("Container(" + containerID + ").ListItem.Property(defaultID)")
+            if xbmc.getCondVisibility("Skin.String(widget-" + skinString + ')'):
+                skinStringContent = xbmc.getInfoLabel("Skin.String(widget-" + skinString + ')')
+           
+        if skinStringContent and not "search" in skinStringContent:
+            if ("$INFO" in skinStringContent or "Activate" in skinStringContent or ":" in skinStringContent):
+                skinStringContent = self.getContentPath(skinStringContent)
+                customWidget = True   
+            if customWidget:
+                 self.win.setProperty("customwidgetcontent", skinStringContent)
+                 self.win.setProperty("activewidget","custom")
+            else:
+                self.win.clearProperty("customwidgetcontent")
+                self.win.setProperty("activewidget",skinStringContent)
+
         else:
-            # Abort was requested while waiting. We should exit
-            xbmc.log('TITANSKIN HELPER SERVICE --> shutdown requested !')
-            #stop the extra threads
-            backgroundsUpdater.stop()
-            libraryMonitor.stop()
-            homeMonitor.stop()
+            self.win.clearProperty("activewidget")
+
+    def setSpotlightWidget(self, containerID):
+        self.win.clearProperty("spotlightwidgetcontent")
+        skinStringContent = ""
+        customWidget = False
+        
+        # workaround for numeric labels (get translated by xbmc)
+        skinString = xbmc.getInfoLabel("Container(" + containerID + ").ListItem.Property(submenuVisibility)")
+        skinString = skinString.replace("num-","")
+        if xbmc.getCondVisibility("Skin.String(spotlightwidget-" + skinString + ')'):
+            skinStringContent = xbmc.getInfoLabel("Skin.String(spotlightwidget-" + skinString + ')')
+        
+        # normal method by getting the defaultID
+        if skinStringContent == "":
+            skinString = xbmc.getInfoLabel("Container(" + containerID + ").ListItem.Property(defaultID)")
+            if xbmc.getCondVisibility("Skin.String(spotlightwidget-" + skinString + ')'):
+                skinStringContent = xbmc.getInfoLabel("Skin.String(spotlightwidget-" + skinString + ')')
+           
+        if skinStringContent and not "search" in skinStringContent:
+            if ("$INFO" in skinStringContent or "Activate" in skinStringContent or ":" in skinStringContent):
+                skinStringContent = self.getContentPath(skinStringContent)
+                customWidget = True
+            self.win.setProperty("spotlightwidgetcontent", skinStringContent)
+
+        else:
+            self.win.clearProperty("spotlightwidgetcontent")        
                               
 
 xbmc.log('titan helper version %s started' % __addonversion__)
